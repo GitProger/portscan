db179f87a4d6a250503bcb3ba943866140665c67
GitProger <51924999+GitProger@users.noreply.github.com>
2019-11-03 15:01:28 +0300

    Create ports.h

---
+++ ports.h
@@ -0,0 +1,11 @@
+#ifndef __PORTS_H_INC
+#define __PORTS_H_INC
+
+#define FTP_PORT      21
+#define SSH_PORT      22
+#define TELNET_PORT   23
+#define SMTP_PORT     25
+#define HTTP_PORT     80
+#define HTTPS_PORT    443
+
+#endif
